beda67398a6dc002b55f1e4c06d40110cee148d6
manyajain27 <jainmanya2701@gmail.com>
2025-05-04 15:06:01 +0530

    fix2

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -98,7 +98,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get("REDIS_URL")],
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
         },
     },
 }
